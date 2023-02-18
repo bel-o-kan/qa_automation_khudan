376b9a81b4cbd80151e4a07067e44aec99f6d2f8
Khudan_Sergii <67749636+bel-o-kan@users.noreply.github.com>
2023-02-18 11:50:30 +0200

    homework

--- lesson_2_khudan.py
+++ lesson_2_khudan.py
@@ -1,25 +1,25 @@
 # task_1.1
 
-#
-# name = input("Введіть ваше імʼя : ")
-# surname = input("Введіть ваше прізвище: ")
-# full_name = name + ' ' + surname
-# print(full_name)
-#
-# # task_1.2
-# print(full_name.title())
-# print(full_name.lower())
-#
-# # task_1.3
-# print((name + " ") * 5)
-#
-# # task_1.4
-# name = input("Введіть ваше імʼя : ")
-# surname = input("Введіть ваше прізвище: ")
-#
-# print(name.strip(), '\n', '\t', surname.strip())
-#
-# full_name_new = name.strip() + ' ' + surname.strip()
+
+name = input("Введіть ваше імʼя : ")
+surname = input("Введіть ваше прізвище: ")
+full_name = name + ' ' + surname
+print(full_name)
+
+# task_1.2
+print(full_name.title())
+print(full_name.lower())
+
+# task_1.3
+print((name + " ") * 5)
+
+# task_1.4
+name = input("Введіть ваше імʼя : ")
+surname = input("Введіть ваше прізвище: ")
+
+print(name.strip(), '\n', '\t', surname.strip())
+
+full_name_new = name.strip() + ' ' + surname.strip()
 
 # task_2.1
 import math
