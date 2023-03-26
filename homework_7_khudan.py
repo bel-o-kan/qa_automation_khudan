8ddfd2a9486faef934182a1ae9cc70a0e7fd5e1d
Khudan_Sergii <67749636+bel-o-kan@users.noreply.github.com>
2023-03-26 22:06:18 +0300

    Homework_7

---
+++ homework_7_khudan.py
@@ -0,0 +1,19 @@
+# task_1
+import re
+
+def check_string(string):
+    pattern = "^[A-Za-z0-9_]*$"
+    return bool(re.match(pattern, string))
+
+# Приклад використання:
+print(check_string("Hello_World123"))  # True
+print(check_string("hello world"))    # False
+
+
+
+# task_2
+strings = ["example (.com)", "github (.com)", "stackoverflow (.com)"]
+
+for string in strings:
+    result = re.sub(r' \(.+?\)', '', string)
+    print(result)
